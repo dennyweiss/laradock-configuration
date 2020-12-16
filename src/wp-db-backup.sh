15c307219cad8e46d85a41ffe753a896d0301c3b
Denny Weiß <>
2020-12-16 17:50:29 +0100

    Add robust wp db backup script for usage with wp only

---
+++ src/wp-db-backup.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+SCRIPT_PATH="${0}"
+BACKUP_DIR="$(dirname "${SCRIPT_PATH}")"
+RESOLVED_PROJECT_ROOT="$(dirname "${BACKUP_DIR}")"
+
+PROJECT_ROOT="${PROJECT_ROOT:-${RESOLVED_PROJECT_ROOT}}"
+cd "${PROJECT_ROOT}" || ( echo -e "ERROR: could not move to project directory '${PROJECT_ROOT}'"; exit 1 )
+
+# shellcheck disable=SC1091
+source .env || ( echo -e "ERROR: could not load .env file"; exit 1 )
+
+WP_BINARY="{WP_BINARY:-wp}"
+
+case "${WP_ENV}" in
+  development)
+  WP_BINARY='wp'
+  ;;
+  staging|production)
+  WP_BINARY="${HOME}/.local/share/wp"
+  ;;
+  *)
+  echo -e "ERROR: WP_ENV '${WP_ENV}' not supported"
+  exit 1
+  ;;
+esac
+
+cd .db || ( echo -e "ERROR: could not move to backup directory '${PROJECT_ROOT}/.db'"; exit 1 )
+
+$WP_BINARY db export "${WP_ENV:-environment-unkown}_$(date "+%Y%m%d-%H%M%S").sql" --add-drop-table \
+|| ( echo -e "ERROR: backup failed"; exit 1 )
